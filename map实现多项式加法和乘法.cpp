63ab35be766af09bea1d925e2a4818a64641b9b0
hkfuwh <31699286+hkfuwh@users.noreply.github.com>
2017-09-16 22:21:24 +0800

    Add files via upload

---
+++ map实现多项式加法和乘法.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include<vector>
+#include<map>
+using namespace std;
+typedef map<int,int>::iterator it;
+typedef map<int,int>::reverse_iterator it1;
+/*运用反向迭代器使之能够降序输出*/
+void print(it1 i,it1 j,map<int,int>::size_type n);
+int main()
+{/*map存储的内容用迭代器访问时默认按照升序访问*/
+    map<int,int> numque1,numque2,ians,jans;
+    /*first 表示指数  second 表示系数*/
+    int m,n;
+    int co,ex;/*co表示系数，ex表示指数*/
+    cin>>m;
+        for(int i=0;i<m;i++)
+        {
+            cin>>co;
+            cin>>ex;
+            numque1[ex]=co;
+        }
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+            cin>>co;
+            cin>>ex;
+            numque2[ex]=co;
+    }
+//    for(it1 i=numque1.rbegin();i!=numque1.rend();i++)
+//        cout<<i->second<<" "<<i->first<<endl;
+//    for(it1 j=numque2.rbegin();j!=numque2.rend();j++)
+//        cout<<j->second<<" "<<j->first<<endl;
+    for(it i=numque1.begin();i!=numque1.end();i++)
+        for(it j=numque2.begin();j!=numque2.end();j++)
+    {
+        ex=i->first+j->first;//指数相加
+        int temp=ians[ex]+i->second*j->second;//系数相乘
+        if(temp!=0)
+            ians[ex]=temp;
+        else
+            ians.erase(ex);
+//        cout<<ex<<" "<<ians[ex]<<endl;
+    }
+    /*加法运算*/
+    it i=numque1.begin(),j=numque2.begin();
+    while(i!=numque1.end()&&j!=numque2.end())
+    {
+        if(i->first==j->first)
+            {/*寻找同类项，如果系数和不为零则存入结果多项式*/
+                 if(i->second+j->second!=0)
+                {
+                    jans[i->first]=i->second+j->second;
+                }
+                i++;j++;
+            }
+
+        else if(i->first<j->first)
+        {
+            jans[i->first]=i->second;
+            i++;
+        }
+            else
+            {
+                jans[j->first]=j->second;
+                j++;
+            }
+    }
+    while(i!=numque1.end())
+    {
+        jans[i->first]=i->second;
+        i++;
+    }
+    while(j!=numque2.end())
+    {
+        jans[j->first]=j->second;
+        j++;
+    }
+    print(ians.rbegin(),ians.rend(),ians.size());
+    print(jans.rbegin(),jans.rend(),jans.size());
+    return 0;
+}
+void print(it1 i,it1 j,map<int,int>::size_type n)
+{
+    map<int,int>::size_type t=1;
+    if(n==0)
+    {
+        cout<<"0 0\n";
+        return;
+    }
+    while(i!=j)
+    {
+        if(t++==n)
+            cout<<i->second<<" "<<i->first<<endl;
+        else
+        cout<<i->second<<" "<<i->first<<" ";
+        i++;
+    }
+
+}
---
+++ reverse.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include<stdio.h>
+using namespace std;
+//typedef map<int,int>::iterator it;
+
+
+/*运用反向迭代器使之能够降序输出*/
+struct node{
+    int data;
+    int next;
+};
+int Reverse(node* list,int pList,int n,int k);
+void printlist(node* l,int head);
+int countlist(node* l,int p);
+int main()
+{
+    node L[100000];
+    int m,n;
+    int firstdata;
+    int ad;
+    cin>>firstdata>>m>>n;
+    for(int i=0;i<m;i++)
+    {
+        cin>>ad;
+        cin>>L[ad].data>>L[ad].next;
+    }
+    int num=countlist(L,firstdata);
+    int newlist=Reverse(L,firstdata,num,n);
+    printlist(L,newlist);
+    return 0;
+}
+/*翻转链表,l表示链表，st第一个元素的地址，n表示元素的总个数，k表示翻转的区段长度
+返回表的头结点*/
+int Reverse(node* list,int pList,int n,int k)
+{
+   int prevNode, currNode, nextNode;
+    prevNode = -1;
+    currNode = pList;
+    nextNode = list[currNode].next;
+    int lastHead, head = -1;
+    int i ,j;
+    for( i = 0; i < n / k; i++){
+            lastHead = head;//记录前一段的（未逆转的）头结点，以便连接到当前段的(未逆转的）尾节点
+                head = currNode;//记录当前段的头结点
+            for(j = 0; j < k; j++) { //k个结点逆转，后一节点指向前一节点
+            list[currNode].next = prevNode;
+                prevNode = currNode;
+                currNode = nextNode;
+                nextNode = list[nextNode].next;
+            }
+            if(i == 0)//第一段逆转后的头结点当作表头返回
+                      pList = prevNode;
+            else
+               list[lastHead].next = prevNode;//连接逆转后的前后两段
+   }
+ list[head].next = currNode;    //不用逆转的剩余部分加上
+    return pList;
+
+}
+void printlist(node* l,int p)
+{
+
+    while(l[p].next!=-1)
+    {
+        printf("%05d %d %05d\n",p,l[p].data,l[p].next);
+        p=l[p].next;
+    }
+    printf("%05d %d %d\n",p,l[p].data,l[p].next);
+}
+int countlist(node* l,int p)//计算列表中的元素个数
+{
+    int cnt=1;
+    while((p=l[p].next)!=-1)
+        cnt++;
+    return cnt;
+}
+
